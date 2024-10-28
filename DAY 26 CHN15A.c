f1974b8c7b2879c239a9a14d5926a4043b2af767
Mactoy1 <ayushkhare654@gmail.com>
2024-10-28 23:14:59 +0530

    Add files via upload

---
+++ DAY 26 CHN15A.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    
+    while (T--) {
+        int N, K, count = 0;
+        scanf("%d %d", &N, &K);
+        
+        for (int i = 0; i < N; i++) {
+            int characteristic;
+            scanf("%d", &characteristic);
+            
+            if ((characteristic + K) % 7 == 0) {
+                count++;
+            }
+        }
+        printf("%d\n", count);
+    }
+    
+    return 0;
+}
